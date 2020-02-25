2a2aa0b3d03f445cd9692d55d8c37c3df2f96424
hahava <hahava5696@gmail.com>
2020-02-25 22:09:28 +0900

    Initial commit

---
+++ src/main/java/me/hahajava/rnserver/RnServerApplication.java
@@ -0,0 +1,13 @@
+package me.hahajava.rnserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hahajava/rnserver/RnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hahajava.rnserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
