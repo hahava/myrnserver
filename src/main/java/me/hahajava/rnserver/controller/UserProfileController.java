6ef6c2350a96de80e3f85471ab7856338df528a7
hahava <hahava5696@gmail.com>
2020-03-02 23:37:40 +0900

    Add user profile create feature

--- src/main/java/me/hahajava/rnserver/controller/UserProfileController.java
+++ src/main/java/me/hahajava/rnserver/controller/UserProfileController.java
@@ -5,19 +5,28 @@ import me.hahajava.rnserver.persistence.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserProfileController {
-
 	@Autowired
 	private UserProfileRepository userProfileRepository;
 
 	@GetMapping("/user/profile/{id}")
 	public ResponseEntity<UserProfile> getUserProfile(@PathVariable Long id) {
 		return new ResponseEntity<>(userProfileRepository.findById(id).get(), HttpStatus.OK);
+	}
+
+	@PostMapping("/user/profile")
+	public ResponseEntity<String> addUserProfile(@RequestBody @Valid UserProfile userProfile, BindingResult br) {
+		if (br.hasErrors()) {
+			return new ResponseEntity<>(br.getAllErrors().get(0).toString(), HttpStatus.BAD_REQUEST);
+		}
+		userProfileRepository.save(userProfile);
+		return new ResponseEntity<>("success", HttpStatus.OK);
 	}
 
 }
--- src/main/java/me/hahajava/rnserver/model/UserProfile.java
+++ src/main/java/me/hahajava/rnserver/model/UserProfile.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -16,9 +17,15 @@ public class UserProfile {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long no;
+
+	@NotNull
 	private String userId;
+	@NotNull
 	private String userPw;
+	@NotNull
 	private String userName;
+	@NotNull
 	private String phoneNo;
+	@NotNull
 	private String token;
 }
