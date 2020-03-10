c8638e2e3a87292842e4512fb5de72e53e180b19
hahava <hahava5696@gmail.com>
2020-03-10 20:53:46 +0900

    Avoid user number change when add new user

--- src/main/java/me/hahajava/rnserver/model/User.java
+++ src/main/java/me/hahajava/rnserver/model/User.java
@@ -1,11 +1,14 @@
 package me.hahajava.rnserver.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
 
 @Entity
 @Getter
@@ -14,6 +17,7 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty(access = READ_ONLY)
 	@Column(name = "no")
 	private Long userNo;
 
