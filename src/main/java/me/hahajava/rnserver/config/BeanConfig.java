be88455a60ee732bd4c3111467ddb27212639d20
hahava <hahava5696@gmail.com>
2020-03-25 23:01:42 +0900

    Change PasswordEncoder to factory method

--- src/main/java/me/hahajava/rnserver/config/BeanConfig.java
+++ src/main/java/me/hahajava/rnserver/config/BeanConfig.java
@@ -2,14 +2,14 @@ package me.hahajava.rnserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
 
 	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 }
