cc9df96f58efa590f0d62ba08643dfba50038076
hahava <hahava5696@gmail.com>
2020-07-22 12:16:42 +0900

    Add user detail service

---
+++ src/main/java/me/hahajava/rnserver/config/security/SecurityDetailService.java
@@ -0,0 +1,24 @@
+package me.hahajava.rnserver.config.security;
+
+import lombok.AllArgsConstructor;
+import me.hahajava.rnserver.model.UserAccount;
+import me.hahajava.rnserver.persistence.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Set;
+
+@AllArgsConstructor
+public class SecurityDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        UserAccount account = userRepository.findById(userId);
+        return new User(account.getId(), account.getPw(), Set.of(new SimpleGrantedAuthority(account.getAuthLevel().name())));
+    }
+}
