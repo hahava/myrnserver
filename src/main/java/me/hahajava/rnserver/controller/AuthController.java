7f886fa276b2a69413a76cc841216102814b3d92
hahava <hahava5696@gmail.com>
2020-07-23 14:39:43 +0900

    Move member register feature to auth controller

--- src/main/java/me/hahajava/rnserver/controller/AuthController.java
+++ src/main/java/me/hahajava/rnserver/controller/AuthController.java
@@ -2,12 +2,15 @@ package me.hahajava.rnserver.controller;
 
 import lombok.AllArgsConstructor;
 import me.hahajava.rnserver.model.UserAccount;
+import me.hahajava.rnserver.persistence.UserRepository;
 import me.hahajava.rnserver.util.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<String> doLogin(@RequestBody UserAccount userAccount) {
@@ -31,6 +36,26 @@ public class AuthController {
 
         final String token = JwtUtil.encodeStringToJwt(userAccount.getId());
         return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> addUserAccount(@RequestBody UserAccount userAccount, BindingResult br) {
+
+        if (br.hasErrors()) {
+            return new ResponseEntity<>(br.getAllErrors().get(0).toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        String cryptPassword = passwordEncoder.encode(userAccount.getPw());
+        userAccount.setPw(cryptPassword);
+
+        UserAccount account = UserAccount.newInstanceForRegister(userAccount);
+        try {
+            userRepository.save(account);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
 }
