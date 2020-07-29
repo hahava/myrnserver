f9b66f48668f9bd61390b4bfaa3b20a672fabbf1
hahava <hahava5696@gmail.com>
2020-07-29 09:43:13 +0900

    Test profile insert

--- src/test/java/me/hahajava/rnserver/repository/ProfileRepositoryTest.java
+++ src/test/java/me/hahajava/rnserver/repository/ProfileRepositoryTest.java
@@ -1,32 +1,42 @@
 package me.hahajava.rnserver.repository;
 
+import me.hahajava.rnserver.model.AuthLevel;
 import me.hahajava.rnserver.model.Profile;
 import me.hahajava.rnserver.model.UserAccount;
 import me.hahajava.rnserver.persistence.ProfileRepository;
-import me.hahajava.rnserver.persistence.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class ProfileRepositoryTest {
 
     @Autowired
     private ProfileRepository profileRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    private UserAccount mockAccount;
+
+    @Before
+    public void setUp() {
+        mockAccount = new UserAccount();
+        mockAccount.setPw("{noop}12345");
+        mockAccount.setId("havana");
+        mockAccount.setAuthLevel(AuthLevel.ADMIN);
+    }
 
     @Test
     public void addProfileTest() {
-        UserAccount account = userRepository.findById("hahava");
-
         Profile profile = new Profile();
-        profile.setNo(null);
         profile.setPhoneNo("010-1234-5678");
-        profile.setUserName("choi");
-        profile.setSelfIntroduce("hello I m choi!");
-        profile.setUserAccount(account);
+        profile.setUserAccount(mockAccount);
+        profile.setSelfIntroduce("my name is havana");
+        profile.setUserName("havana");
 
         profileRepository.save(profile);
     }
